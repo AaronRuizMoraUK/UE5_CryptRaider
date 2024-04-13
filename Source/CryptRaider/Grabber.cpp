a837a5a3ea867db6569a6acb5ed9132d588f04ef
AaronRuizMoraUK <aaronruizmora@gmail.com>
2024-04-13 17:04:22 +0100

    Add functions to Grabber that can be called from Blueprints. Add and use UPhysicsHandleComponent to our BP Player to grab and release another object in the world.

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -3,6 +3,7 @@
 
 #include "Grabber.h"
 #include "Engine/World.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
@@ -20,9 +21,6 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
 
@@ -31,24 +29,154 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	const FVector StartLocation = GetComponentLocation();
-	const FVector EndLocation = StartLocation + GetForwardVector() * MaxGrabDistance;
-	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red);
-
-	if (FHitResult HitResult;
-		GetWorld()->SweepSingleByChannel(
-			HitResult,
-			StartLocation,
-			EndLocation,
-			FQuat::Identity,
-			ECC_GameTraceChannel2,
-			FCollisionShape::MakeSphere(GrabSphereRadius)
-		))
+	// Early return if there si no physics handle
+	if (!GetPhysicsHandle())
 	{
-		UE_LOG(LogTemp, Display, TEXT("Hit %s"), *HitResult.GetActor()->GetActorNameOrLabel());
+		return;
+	}
+
+	if (auto GrabbedComponent = GetGrabbedComponent())
+	{
+		if (FVector::Distance(GrabbedComponent->GetComponentLocation(), GetComponentLocation()) > DropDistance)
+		{
+			// Release the object if it gets too far away
+			Release();
+		}
+		else
+		{
+			// Update grabbed component location and rotation
+			const FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+			GetPhysicsHandle()->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+		}
 	}
 	else
 	{
-		UE_LOG(LogTemp, Display, TEXT("No actor hit"));
+		if (Debug)
+		{
+			// Draw sphere at max grab distance. Red if it's colliding, blue if it's not.
+			DrawDebugSphere(
+				GetWorld(),
+				GetComponentLocation() + GetForwardVector() * MaxGrabDistance,
+				GrabSphereRadius,
+				20,
+				CheckHit().has_value() ? FColor::Red : FColor::Blue
+			);
+		}
 	}
+}
+
+
+void UGrabber::Grab()
+{
+	// Early return if there si no physics handle
+	if (!GetPhysicsHandle())
+	{
+		return;
+	}
+
+	// Do nothing if it's grabbing an object already
+	if (IsGrabbing())
+	{
+		return;
+	}
+
+	if (auto HitResult = CheckHit();
+		HitResult.has_value())
+	{
+		if (Debug)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Grabbing actor: %s"), *HitResult->GetActor()->GetActorNameOrLabel());
+		}
+
+		auto HitComponent = HitResult->GetComponent();
+
+		// Wake hit component's physics so it's ready to be grabbed.
+		HitComponent->WakeAllRigidBodies();
+
+		GetPhysicsHandle()->GrabComponentAtLocationWithRotation(
+			HitComponent,
+			NAME_None, 
+			HitResult->ImpactPoint, 
+			GetComponentRotation()
+		);
+	}
+	else
+	{
+		if (Debug)
+		{
+			UE_LOG(LogTemp, Display, TEXT("No actor grabbed!"));
+		}
+	}
+}
+
+
+void UGrabber::Release()
+{
+	// Early return if there si no physics handle
+	if (!GetPhysicsHandle())
+	{
+		return;
+	}
+
+	if (auto GrabbedComponent = GetGrabbedComponent())
+	{
+		if (Debug)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Releasing actor: %s"), *GrabbedComponent->GetOwner()->GetActorNameOrLabel());
+		}
+
+		// Wake up the grabbed component so it's physics are active when it's released
+		GrabbedComponent->WakeAllRigidBodies();
+
+		GetPhysicsHandle()->ReleaseComponent();
+	}
+	else
+	{
+		if (Debug)
+		{
+			UE_LOG(LogTemp, Display, TEXT("No actor to release!"));
+		}
+	}
+}
+
+
+bool UGrabber::IsGrabbing() const
+{
+	return GetGrabbedComponent() != nullptr;
+}
+
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	return GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+}
+
+
+UPrimitiveComponent* UGrabber::GetGrabbedComponent() const
+{
+	auto PhysicsHandle = GetPhysicsHandle();
+	return PhysicsHandle
+		? PhysicsHandle->GetGrabbedComponent()
+		: nullptr;
+}
+
+
+std::optional<FHitResult> UGrabber::CheckHit() const
+{
+	const FVector StartLocation = GetComponentLocation();
+	const FVector EndLocation = StartLocation + GetForwardVector() * MaxGrabDistance;
+
+	FHitResult HitResult;
+	bool Hit = GetWorld()->SweepSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(GrabSphereRadius)
+	);
+
+	return Hit 
+		? std::optional<FHitResult>(std::move(HitResult)) 
+		: std::nullopt;
 }
--- Source/CryptRaider/Grabber.h
+++ Source/CryptRaider/Grabber.h
@@ -2,10 +2,14 @@
 
 #pragma once
 
+#include <optional>
+
 #include "CoreMinimal.h"
 #include "Components/SceneComponent.h"
 #include "Grabber.generated.h"
 
+class UPhysicsHandleComponent;
+class UPrimitiveComponent;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UGrabber : public USceneComponent
@@ -20,14 +24,38 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintCallable)
+	void Grab();
+
+	UFUNCTION(BlueprintCallable)
+	void Release();
+
+	UFUNCTION(BlueprintCallable)
+	bool IsGrabbing() const;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
 	UPROPERTY(EditAnywhere)
-	float MaxGrabDistance = 400.0f;
+	float MaxGrabDistance = 330.0f;
 
 	UPROPERTY(EditAnywhere)
 	float GrabSphereRadius = 100.0f;
+
+	UPROPERTY(EditAnywhere)
+	float HoldDistance = 100.0f;
+
+	UPROPERTY(EditAnywhere)
+	float DropDistance = 500.0f;
+
+	UPROPERTY(EditAnywhere)
+	bool Debug = false;
+
+	UPhysicsHandleComponent* GetPhysicsHandle() const;
+
+	UPrimitiveComponent* GetGrabbedComponent() const;
+
+	std::optional<FHitResult> CheckHit() const;
 };
